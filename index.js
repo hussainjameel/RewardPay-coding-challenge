9ed42a66940b0aaa791f7995cc4066c9ceefa660
Hussain Jameel <syedhussainjameel@gmail.com>
2024-12-07 22:52:40 +1100

    Expenses Calculated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const dataFile = require('./data.json');
-console.log(dataFile);
+// console.log(dataFile);
 
 const records = dataFile.data;
 
@@ -12,4 +12,11 @@ const calculateRevenue = (data) => {
   console.log("Revenue: ", revenue)  
 }
 
+const calculateExpenses = (data) => {
+  const expenses = data
+    .filter(item => item.account_category === 'expense')
+    .reduce((sum, item) => sum + item.total_value, 0);
+  console.log("Expenses", expenses)  
+}
 calculateRevenue(records)
+calculateExpenses(records)
